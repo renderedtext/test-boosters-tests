6c9d26f70be8e9e04cca53091ae2fc470beaddd6
Igor Šarčević <igor@renderedtext.com>
2017-07-20 15:09:39 +0200

    Add a shared example for rspec project

--- rspec_project/spec/rspec_project_spec.rb
+++ rspec_project/spec/rspec_project_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
+require "shared_examples/a"
 
 describe RspecProject do
+  it_behaves_like "a test with shared examples"
+
   it 'has a version number' do
     expect(RspecProject::VERSION).not_to be nil
   end
---
+++ rspec_project/spec/shared_examples/a.rb
@@ -0,0 +1,5 @@
+shared_examples "a test with shared examples" do
+  it "does something interesting" do
+    expect(1+1).to eq(2)
+  end
+end
